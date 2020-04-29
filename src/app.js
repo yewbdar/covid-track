8aacbca9286470e69fc0402e30e2750c17d315b4
yewbdar <Yewbdar.Grima@Gmail.com>
2020-04-29 14:52:44 -0500

    component added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import React ,{Component} from 'react'
 
 import {Cards , Charts , CountyPicker } from './component'
-
+import style from './App.module.css'
+import {fechData} from './Api' 
 class App  extends Component {
+    state = {
+        data:{}
+    }
+    async componentDidMount(){
+        const newData = await fechData()
+        console.log(newData)
+        this.setState({data:newData})
+    }
     render(){
-        return (<div> hi
-            <Cards/> 
+        return (<div className={style.container} > 
+        {console.log(this.state.data)}
+            <Cards data={this.state.data}/> 
+           
             <CountyPicker/>
              <Charts/>
         </div>)
--- src/component/Cards/Cards.jsx
+++ src/component/Cards/Cards.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
-const Cards = () => {
+import style from './cards.module.css'
+import {Card , CardContent ,Typography ,Grid } from '@material-ui/core'
+import CountUp from 'react-countup'
+const Cards = ({data:{confirmed,recovered,deaths,lastUpdate}}) => {
+    // console.log('props',confirmed.value)
+    // {data:{confirmed,recovered,deaths,lastUpdate}}
+    if(!confirmed){
+        return 'Loding...'
+    }
     return (
-         <h1>Cards </h1>
+         <div className={style.container}>
+             <Grid container spacing={3} justify="center">
+               <Grid item component={Card} >
+                   <CardContent>
+                       <Typography color='textSecondary' gutterBottom> infected</Typography>
+                       <Typography variant='h5'>
+                         <CountUp start={0} end={confirmed.value} duration={2.5} separator=','></CountUp>
+                       </Typography>
+                       <Typography color='textSecondary'> {new Date(lastUpdate).toDateString()}
+                       </Typography>
+                       <Typography variant='body2'>number of active case of COVID-19</Typography>
+                   </CardContent>
+               </Grid>
+             </Grid>
+         </div>
     )
 }
 export default Cards
