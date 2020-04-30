25be7fc418efaafd996543a9d54e7c44a5a0fd54
yewbdar <Yewbdar.Grima@Gmail.com>
2020-04-30 15:56:50 -0500

    component added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React ,{Component} from 'react'
 
 import {Cards , Charts , CountyPicker } from './component'
 import style from './App.module.css'
-import {fechData} from './Api' 
+import {fechData } from './Api' 
 class App  extends Component {
     state = {
         data:{},
@@ -10,19 +10,23 @@ class App  extends Component {
     }
     async componentDidMount(){
         const newData = await fechData()
-        // console.log(newData)
+        
         this.setState({data:newData})
     }
-    handleChange = (cuntry) =>{
+    handleChange = async (cuntry) =>{
+        const newData = await fechData(cuntry)
+        
+        this.setState({data:newData ,country:cuntry})
          console.log(cuntry)
     }
     render(){
+        const { data , country} = this.state
         return (<div className={style.container} > 
         {console.log(this.state.data)}
             <Cards data={this.state.data}/> 
            
             <CountyPicker handleChange={this.handleChange}/>
-             <Charts/>
+             <Charts  data={data } country={country}/>
         </div>)
     }
 }
--- src/component/Charts/Chart.jsx
+++ src/component/Charts/Chart.jsx
@@ -1,9 +1,11 @@
 import React ,{ useEffect ,useState} from "react"
-import { fecthDialyData } from '../../Api'
+import { fecthDialyData , fethchDataByCountry } from '../../Api'
 import {Line , Bar } from 'react-chartjs-2'
 import style from './chart.module.css'
 import { interfaceExtends } from "@babel/types"
-const Chart = () =>{
+const Chart = (props) =>{
+  const {data:{confirmed , recovered, deaths} , country} = props;
+ 
      const [dailyData , setDailyData] = useState([])
 
     useEffect(() => {
@@ -13,7 +15,7 @@ const Chart = () =>{
         // console.log(dailyData)
          fetchApi();
         
-    })
+    },[])
 
     const lineChart = (
         dailyData.length?
@@ -37,9 +39,30 @@ const Chart = () =>{
           }}
         />
         ):null)
+
+        const BarChart = (
+          
+          confirmed? (
+            <Bar 
+               data={{
+                 labels:['confirmed', 'recovered','deaths'],
+                 datasets:[{
+                   label:'people',
+                   backgroundColor:['rgba(0, 0, 255, 0.5)','rgba(0, 255, 0, 0.5)','rgba(255, 0, 0, 0.5)'],
+                   data:[confirmed.value , recovered.value, deaths.value]
+                 }]
+               }}
+               options={{
+                 legend:{display:false},
+                 title:{display:true, title:`current state in ${country}`}
+               }}
+            />
+          ):null
+          
+        )
     return (
         <div className={style.container}>
-            {lineChart}
+            {country  ? BarChart : lineChart}
         </div>
     )
 }
